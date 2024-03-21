d41def916497d26cf5dedeade79cbd6bbdeb33e3
OgGhostJelly <johnnycambodia09@gmail.com>
2024-03-21 14:44:22 +0700

    Smarter zombies and walls

--- objects/bullets/bullet/bullet.gd
+++ objects/bullets/bullet/bullet.gd
@@ -22,3 +22,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_wall_detector_body_entered(_body: Node2D) -> void:
+	queue_free()
--- objects/enemies/enemy/enemy.gd
+++ objects/enemies/enemy/enemy.gd
@@ -6,11 +6,19 @@ extends Character2D
 @onready var context_steerer: ContextSteerer = $ContextSteerer
 @onready var death_audio: AudioStreamPlayer = $DeathAudio
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var sight_detector: Area2D = $SightDetector
+@onready var gun_sight_detector: Area2D = $GunSightDetector
+@onready var player_sight_detector: Area2D = $PlayerSightDetector
+
+var last_seen_position: Vector2
+var last_seen_is_valid: bool
+
+var random_moving: bool
+var random_direction: Vector2
 
 
 func _ready() -> void:
 	super()
+	last_seen_position = global_position
 	animated_sprite.play(&"walk0")
 	
 	health.value_changed.connect(func():
@@ -18,11 +26,36 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	var player: Node2D = get_player()
+	var target_direction: Vector2
 	
-	context_steerer.target_direction = global_position.direction_to(player.global_position)
+	for node in player_sight_detector.get_overlapping_bodies():
+		var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+		var query = PhysicsRayQueryParameters2D.create(
+			global_position,
+			node.global_position,
+			0b10000)
+		query.hit_from_inside = false
+		var result = space_state.intersect_ray(query)
+		
+		if not result.is_empty():
+			continue
+		
+		last_seen_position = node.global_position
+		target_direction = global_position.direction_to(node.global_position)
+		last_seen_is_valid = true
+		break
 	
-	for area in sight_detector.get_overlapping_areas():
+	if target_direction.is_zero_approx() and last_seen_is_valid:
+		target_direction = global_position.direction_to(last_seen_position)
+		if global_position.distance_squared_to(last_seen_position) < 16.0 ** 2:
+			last_seen_is_valid = false
+	
+	if target_direction.is_zero_approx() and random_moving:
+		target_direction = random_direction.normalized()
+	
+	context_steerer.target_direction = target_direction
+	
+	for area in gun_sight_detector.get_overlapping_areas():
 		context_steerer.target_direction = context_steerer.target_direction.rotated(PI/4.0)
 	
 	context_steerer.update_direction()
@@ -39,11 +72,6 @@ func _process(_delta: float) -> void:
 	move_and_slide()
 
 
-func get_player() -> CharacterBody2D:
-	var players: Array[Node] = get_tree().get_nodes_in_group(&"player")
-	return players[0]
-
-
 func _die() -> void:
 	animated_sprite.play(&"death")
 	animated_sprite.reparent(get_parent())
@@ -58,3 +86,8 @@ func _die() -> void:
 
 func _on_hit_detector_hurt(_hitbox: HitInfo2D) -> void:
 	health.value -= 1
+
+
+func _on_random_move_timer_timeout() -> void:
+	random_moving = randf() > 0.5
+	random_direction = Vector2(randf(), randf()).normalized()
