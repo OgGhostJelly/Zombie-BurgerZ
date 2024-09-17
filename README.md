a9c38b22f2baf4d51f5d2e0c7eb35206ceb4e641
OgGhostJelly <johnnycambodia09@gmail.com>
2024-09-17 08:01:15 +0700

    Tweaked splitter zombie

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-- custom mode when clicking boards in the background
+- needs some re-architecting to support json modifying or the like and some below features
+
+- randomize loadout option
+- update enemy wave/spawn pattern
 - achievement for spamming shop buttons
 
-- make permanent changes to the map at the start of the game and live with the changes you made
-- turn zombies into minions
+- could have secondary weapons or augments
 
-- (could have secondary weapons?)
+- (custom mode when clicking boards in the background)
+- (make permanent changes to the map at the start of the game and live with the changes you made?)
+- (turn zombies into minions?)
 - (bestiary)
 - (slow mo bullet time when shooting)
--- objects/enemy/splitter_zombie.gd
+++ objects/enemy/splitter_zombie.gd
@@ -1,18 +1,38 @@
 extends Enemy
 
 
+var should_split: bool = true
+
+
 func _die() -> void:
 	if is_dead:
 		return
 	
 	is_dead = true
 	
-	var obj: Node2D = preload("res://objects/enemy/splitter_half_zombie.tscn").instantiate()
-	obj.global_position = global_position - Vector2(8.0, 0.0)
-	call_deferred(&"add_sibling", obj)
-	
-	obj = preload("res://objects/enemy/splitter_half_zombie.tscn").instantiate()
-	obj.global_position = global_position + Vector2(8.0, 0.0)
-	call_deferred(&"add_sibling", obj)
+	spawn_split(Vector2(-8.0, 0.0))
+	spawn_split(Vector2( 8.0, 0.0))
 	
 	queue_free()
+
+
+func spawn_split(offset: Vector2) -> void:
+	var scene: PackedScene = preload("res://objects/enemy/splitter_half_zombie.tscn")
+	var obj: Node2D = scene.instantiate()
+	obj.global_position = global_position + offset
+	
+	if not should_split:
+		obj.ready.connect(func():
+			obj.health.value = 0)
+	
+	call_deferred(&"add_sibling", obj)
+
+
+func _on_hit_detector_hurt(info: HitInfo2D) -> void:
+	var bullet: Bullet = info.hitbox as Bullet
+	if bullet != null and health.max_value == bullet.pierce:
+		should_split = false
+	else:
+		should_split = true
+	
+	super(info)
